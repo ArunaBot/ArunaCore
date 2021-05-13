430f4d7ca23de9afeb01a25dd1d2160c1fecabea
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2021-05-13 19:42:45 -0300

    Fix Logger when message is Object

--- src/Utils/logger.js
+++ src/Utils/logger.js
@@ -32,26 +32,31 @@ class logger {
     }
 
     fatal(Message) {
+        if (typeof Message === 'object') Message = JSON.stringify(Message);
         // eslint-disable-next-line max-len
         console.trace(chalk.bgWhite(chalk.red(Message.toString().split(' ')[0].toLowerCase().includes('error') ? `[${dateParser.getTime()}] [${this.prefix}] Fatal ${Message}` : `[${dateParser.getTime()}] [${this.prefix}] Fatal: ${Message}`)));
         process.exit(5);
     }
     
     error(Message) {
+        if (typeof Message === 'object') Message = JSON.stringify(Message);
         // eslint-disable-next-line max-len
         console.error(chalk.red(Message.toString().split(' ')[0].toLowerCase().includes('error') ? `[${dateParser.getTime()}] [${this.prefix}] ${Message}` : `[${dateParser.getTime()}] [${this.prefix}] Error: ${Message}`));
     }
     
     warn(Message) {
+        if (typeof Message === 'object') Message = JSON.stringify(Message);
         console.warn(chalk.keyword('orange')(`[${dateParser.getTime()}] [${this.prefix}] Warn: ${Message}`));
     }
 
     info(Message) {
+        if (typeof Message === 'object') Message = JSON.stringify(Message);
         console.info(chalk.blueBright(`[${dateParser.getTime()}] [${this.prefix}] `) + `Info: ${Message}`);
     }
 
     debug(Message) {
         if (this.debugActive) {
+            if (typeof Message === 'object') Message = JSON.stringify(Message);
             console.debug(chalk.gray(`[${dateParser.getTime()}] [${this.prefix}] Debug: `) + chalk.hex('#AAA')(Message));
         }
     }
