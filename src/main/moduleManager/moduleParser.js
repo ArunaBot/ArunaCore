25e89ba1bc53b5c7f3118cba00ec2e1d3bbb0e1a
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2021-07-10 19:35:02 -0300

    (fix): crash when .arunamodule doen't have main file

--- src/main/moduleManager/moduleParser.js
+++ src/main/moduleManager/moduleParser.js
@@ -39,6 +39,8 @@ class ModuleParser {
             function parserV1() {
                 finalObj.name = obj.moduleInfo.name;
                 finalObj.version = obj.moduleInfo.version;
+                finalObj.description = obj.moduleInfo.description;
+                finalObj.main = obj.moduleInfo.main || 'index.js';
                 finalObj.author = obj.moduleInfo.author;
                 finalObj.license = obj.moduleInfo.license;
                 finalObj.repository = obj.moduleInfo.repository;
