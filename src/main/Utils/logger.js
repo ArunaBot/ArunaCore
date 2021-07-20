37584e7f28d95525cb16581c4cd1b0571cf4ec3c
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2021-07-20 15:34:27 -0300

    (docs): add comments

--- src/main/Utils/logger.js
+++ src/main/Utils/logger.js
@@ -24,13 +24,24 @@ const path = require('path');
 var chalk = require('chalk');
 var dateParser = require(path.resolve(__dirname,'DateParser'));
 
-
+/**
+ * Logger Class
+ * @constructor
+ * @optional @param {Object} [config] - Configuration object
+ * @optional @param {Boolean} [config.debug] - Enable debug logging
+ * @optional @param {String} [config.prefix] - Prefix for log messages
+ */
 class logger {
     constructor(options) {
         this.debugActive = options ? options.debug : false;
         this.prefix = options ? options.prefix : false;
     }
 
+    /**
+     * Crash the process with a message
+     * @param {String} [message] - The message to crash with
+     * @param {Object} [data] - Additional data to crash with (replace message)
+     */
     fatal(Message) {
         if (typeof Message === 'object') Message = JSON.stringify(Message);
         // eslint-disable-next-line max-len
@@ -38,26 +49,53 @@ class logger {
         process.exit(5);
     }
     
+    /**
+     * Log a error
+     * @param {String} [message] - The message to log
+     * @param {Object} [data] - Additional data to log (replace message)
+     */
     error(Message) {
         if (typeof Message === 'object') Message = JSON.stringify(Message);
         // eslint-disable-next-line max-len
         console.error(chalk.red(Message.toString().split(' ')[0].toLowerCase().includes('error') ? `[${dateParser.getTime()}] [${this.prefix}] ${Message}` : `[${dateParser.getTime()}] [${this.prefix}] Error: ${Message}`));
     }
     
+    /**
+     * Log a warning
+     * @param {String} [message] - The message to log
+     * @param {Object} [data] - Additional data to log (replace message)
+     */
     warn(Message) {
         if (typeof Message === 'object') Message = JSON.stringify(Message);
         console.warn(chalk.keyword('orange')(`[${dateParser.getTime()}] [${this.prefix}] Warn: ${Message}`));
     }
 
+    /**
+     * Log a info
+     * @param {String} [message] - The message to log
+     * @param {Object} [data] - Additional data to log (replace message)
+     */
     info(Message) {
         if (typeof Message === 'object') Message = JSON.stringify(Message);
         console.info(chalk.blueBright(`[${dateParser.getTime()}] [${this.prefix}] `) + `Info: ${Message}`);
     }
 
+    /**
+     * Triggers "info"
+     * @deprecated
+     * @deorecationDetail Use "info" instead
+     * @param {String} [message] - The message to log
+     * @param {Object} [data] - Additional data to log (replace message)
+     */
     log(Message) {
         this.info(Message);
     }
 
+    /**
+     * Log a debug
+     * @param {String} [message] - The message to log
+     * @param {Object} [data] - Additional data to log (replace message)
+     */
     debug(Message) {
         if (this.debugActive) {
             if (typeof Message === 'object') Message = JSON.stringify(Message);
