01931daddc1bc420378d2cb1e876a7296a77762f
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2022-02-22 23:09:45 -0300

    (fix): eslint on bundle

--- bundle/src/scripts/runESLint.js
+++ bundle/src/scripts/runESLint.js
@@ -1,11 +1,10 @@
-const { execSync } = require("child_process");
-const path = require("path");
-const fs = require("fs");
+const { execSync } = require('child_process');
+const path = require('path');
 
-const parts = ["api", "cli", "common", "core", "database", "http", "websocket"];
+const parts = ['api', 'bundle', 'cli', 'common', 'core', 'database', 'http', 'websocket'];
 
 for (const part of parts) {
   console.log(`Running ESLint on ${part}...`);
-  execSync('eslint ./src --fix', { cwd: path.join(__dirname, "..", "..", "..", part) });
-  console.log(`Finished ESLint execution on ${part}!`)
+  execSync('eslint ./src --fix', { cwd: path.join(__dirname, '..', '..', '..', part) });
+  console.log(`Finished ESLint execution on ${part}!`);
 }
