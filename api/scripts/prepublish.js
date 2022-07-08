b1bc75b1a07f1455b261498d506cad1b5a527bc5
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2022-07-08 11:45:17 -0300

    (fix): types file

--- api/scripts/prepublish.js
+++ api/scripts/prepublish.js
@@ -3,11 +3,15 @@ const fs = require('fs');
 const packageRaw = fs.readFileSync('./out/package.json', 'utf8');
 const packageJson = JSON.parse(packageRaw);
 
-packageJson.types = 'build/src/index.ts';
+packageJson.types = 'build/src/index.d.ts';
 
 delete packageJson.scripts;
 
 fs.writeFileSync('./out/package.json', JSON.stringify(packageJson, null, 2));
+
+if (fs.existsSync('./out/build/tsconfig.tsbuildinfo')) {
+  fs.unlinkSync('./out/build/tsconfig.tsbuildinfo');
+}
 
 fs.copyFileSync('./.npmignore', './out/.npmignore');
 fs.copyFileSync('../LICENSE', './out/LICENSE');
