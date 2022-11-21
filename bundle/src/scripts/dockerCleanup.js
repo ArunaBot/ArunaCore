231b47d3d9105b2d3e6adac81651a531cceaf401
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2022-11-21 19:18:31 -0300

    [docker] (feat): add docker cleanup script

---
+++ bundle/src/scripts/dockerCleanup.js
@@ -0,0 +1,18 @@
+const { execSync } = require('child_process');
+const bundle = require('../../package.json');
+const path = require('path');
+const fs = require('fs');
+
+const parts = ['api', 'cli', 'core', 'http', 'websocket'];
+
+for (const part of parts) {
+  const { devDependencies } = require(path.join('..', '..', '..', part, 'package.json'));
+  console.log(`Installing ${part}`);
+  bundle.devDependencies = { ...bundle.devDependencies, ...devDependencies };
+  execSync('npm ci --ignore-scripts', { cwd: path.join(__dirname, '..', '..', '..', part), stdio: 'inherit' });
+  console.log(`Installed ${part}`);
+}
+
+fs.writeFileSync(path.join(__dirname, '..', '..', 'package.json'), JSON.stringify(bundle, null, '\t'), { encoding: 'utf8' });
+
+execSync('npm ci --ignore-scripts', { cwd: path.join(__dirname, '..', '..'), stdio: 'inherit' });
