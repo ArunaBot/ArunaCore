8736aa081e07ef2d2ffa89748ffd937986fc16d4
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2023-07-20 14:22:56 -0300

    (fix): parser issues

--- api/src/websocket/parser.ts
+++ api/src/websocket/parser.ts
@@ -1,69 +1,101 @@
 import { IMessage } from '../interfaces';
 
 export class WebSocketParser {
-  private options: any;
-
-  constructor(options?: any) {
-    this.options = options;
-  }
-
+  /**
+   * Parse a WebSocket message string and extract its components.
+   * @param message - The WebSocket message string to parse.
+   * @returns The parsed message object or null if the message is invalid.
+   */
   public parse(message: string): IMessage|null {
-    if (!message.startsWith(':')) return null;
+    try {
+      if (!message.startsWith(':')) return null;
 
-    const split = message.split(' ');
+      const split = message.split(' ');
 
-    if (split.length > 1) {
-      const parsedArg0: string[] = split[0].split('-');
+      if (split.length > 1) {
+        const parsedArg0: string[] = split[0].split('-');
 
-      var from: string = split[0].substring(1);
-      var type: string|undefined;
+        var from: string = split[0].substring(1);
+        var type: string|undefined;
 
-      if (parsedArg0.length >= 2) {
-        from = split[0].split('-')[1];
-        type = split[0].split('-')[0].substring(1);
-      }
-
-      const command: string = split[1];
-
-      var args: string[] = [];
-
-      var to: string|undefined;
-
-      if (split[2].startsWith(':')) {
-        // Args
-        args = split.slice(2, split.length);
-      } else {
-        // To
-        to = split[2];
-        args = split.slice(3, split.length);
-      }
-
-      args[0] = args[0].substring(1);
-
-      var secureKey: string|undefined;
-      var targetKey: string|undefined;
-      if (args[0]?.startsWith('key:')) {
-        secureKey = args[0].split(':')[1];
-        args.shift();
-        if (args[0]?.startsWith('targetKey:')) {
-          targetKey = args[0].split(':')[1];
-          args.shift();
-        } else {
-          targetKey = secureKey;
+        if (parsedArg0.length >= 2) {
+          [from, type] = parsedArg0;
+          from = from.substring(1);
         }
+
+        const command: string = split[1];
+
+        var args: string[] = [];
+
+        var to: string|undefined;
+
+        if (split[2].startsWith(':')) {
+        // Args
+          args = split.slice(2, split.length);
+        } else {
+        // To
+          to = split[2];
+          args = split.slice(3, split.length);
+        }
+
+        args[0] = args[0].substring(1);
+
+        var secureKey: string|undefined;
+        var targetKey: string|undefined;
+        if (args[0]?.startsWith('key:')) {
+          secureKey = args[0].split(':')[1];
+          args.shift();
+          if (args[0]?.startsWith('targetKey:')) {
+            targetKey = args[0].split(':')[1];
+            args.shift();
+          } else {
+            targetKey = secureKey;
+          }
+        }
+
+        return { from, type, command, to, secureKey, targetKey, args };
       }
 
-      return { from, type, command, to, secureKey, targetKey, args };
+      return null;
+    } catch (err) {
+      return null;
     }
-
-    return null;
   }
 
+  /**
+   * Create the message object by the given properties.
+   * @param from - The 'from' property of the message.
+   * @param command - The 'command' property of the message.
+   * @param args - The 'args' property of the message.
+   * @param to - The optional 'to' property of the message.
+   * @param type - The optional 'type' property of the message.
+   * @returns Message object.
+   */
   public format(from: string, command: string, args: string[], to?: string, type?: string): IMessage {
     return { from, type, command, to, args };
   }
 
+  /**
+   * Convert a WebSocket message object into a message string.
+   * @param message - The message object to convert.
+   * @returns The WebSocket message string.
+   */
   public toString(message: IMessage): string {
     return `:${message.from}${message.type ? `-${message.type}` : ''} ${message.command} ${message.to ? message.to + ' ' : ''}:${message.args.join(' ')}`;
+  }
+
+  /**
+   * Format and convert a WebSocket message object into a message string.
+   * @param from - The 'from' property of the message.
+   * @param command - The 'command' property of the message.
+   * @param args - The 'args' property of the message.
+   * @param to - The optional 'to' property of the message.
+   * @param type - The optional 'type' property of the message.
+   * @returns The WebSocket message string.
+   * @see WebSocketParser.format
+   * @see WebSocketParser.toString
+   */
+  public formatToString(from: string, command: string, args: string[], to?: string, type?: string): string {
+    return this.toString(this.format(from, command, args, to, type));
   }
 }
