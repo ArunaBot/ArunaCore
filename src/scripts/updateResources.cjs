aa5928ef1dd5f1d7cbff38de9d6e2962cad7afa7
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2025-05-31 12:50:18 -0300

    fix: remove legacy loader flag from start and test

--- src/scripts/updateResources.cjs
+++ src/scripts/updateResources.cjs
@@ -10,5 +10,9 @@ fs.cpSync(src, dest, { recursive: true, force: true, preserveTimestamps: true })
 // Also copy ./loader.js to build/nodejs/main/loader.js
 const loaderSrc = path.resolve(__dirname, 'loader.js');
 const loaderDest = path.resolve(__dirname, '..', '..', 'build', 'nodejs', 'src', 'main', 'loader.js');
-
 fs.cpSync(loaderSrc, loaderDest, { force: true, preserveTimestamps: true });
+
+// Copy registerLoader.js to build/nodejs/src/main/registerLoader.js
+const registerLoaderSrc = path.resolve(__dirname, 'registerLoader.js');
+const registerLoaderDest = path.resolve(__dirname, '..', '..', 'build', 'nodejs', 'src', 'main', 'registerLoader.js');
+fs.cpSync(registerLoaderSrc, registerLoaderDest, { force: true, preserveTimestamps: true });
